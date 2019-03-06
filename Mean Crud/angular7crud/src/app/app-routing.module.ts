aa7de9811c4598d033458c52e86e3a90debf6e47
ilhan <ilhandemirtepe@gmail.com>
2019-03-06 13:50:17 +0300

    angular 7 node js crud
    node js ,angular 7,mongo db,
    node js,bootstrap 4 crud example

---
+++ Mean Crud/angular7crud/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { GstGetComponent } from './gst-get/gst-get.component';
+import { GstEditComponent } from './gst-edit/gst-edit.component';
+import { GstAddComponent } from './gst-add/gst-add.component';
+
+const routes: Routes = [
+  {
+    path: 'business/create',
+    component: GstAddComponent
+  },
+  {
+    path: 'business/edit/:id',
+    component: GstEditComponent
+  },
+  {
+    path: 'business',
+    component: GstGetComponent
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
